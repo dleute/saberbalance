a0cdd2f0ef92ca80a4994c1fcd599c0328d76765
Derrek <dleute@users.noreply.github.com>
2025-05-04 20:02:03 -0400

    trying nested component\

---
+++ components/main.vue
@@ -0,0 +1,19 @@
+<template>
+    <hello-world></hello-world>
+    <h3>And that's it!</h3>
+</template>
+  
+<script setup>
+import HelloWorld from './hello.vue'
+
+export default {
+    name: 'MainApp',
+}
+</script>
+  
+<style scoped>
+h3 {
+    color: white;
+    font-size: 1rem;
+}
+</style>
