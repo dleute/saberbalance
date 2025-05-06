7ab7256efb7b8f84e802015ed6aa484279189db5
Derrek <dleute@users.noreply.github.com>
2025-05-05 20:22:50 -0400

    chips are down

--- components/saber.vue
+++ components/saber.vue
@@ -1,9 +1,9 @@
 <template>
-    <v-card title="Saber" text="uh dont need text">
-  <v-card-actions>
-    <v-btn>Click me</v-btn>
-  </v-card-actions>
-</v-card>
+  <v-card title="Saber" text="uh dont need text" elevation="8" hover>
+    <v-card-actions>
+      <v-btn>Click me</v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script setup>
