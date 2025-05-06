ba97efffa0de0fc7ea5b429e8e3d3f247f7f2492
Derrek <dleute@users.noreply.github.com>
2025-05-05 22:33:03 -0400

    testing new layout

--- components/main.vue
+++ components/main.vue
@@ -1,7 +1,28 @@
 <template>
-    <hello-world></hello-world>
-    <h3>And that's it!</h3>
-    <saber-list></saber-list>
+  <v-layout class="rounded rounded-md border">
+    <v-app-bar title="Application bar"></v-app-bar>
+
+    <v-navigation-drawer>
+      <v-list nav>
+        <v-list-item title="Navigation drawer" link></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="d-flex align-center justify-center" height="300">
+      <v-container>
+        <v-sheet
+          border="dashed md"
+          color="surface-light"
+          height="200"
+          rounded="lg"
+          width="100%"
+        >
+        <saber-list></saber-list>
+        </v-sheet>
+      </v-container>
+    </v-main>
+  </v-layout>
+    
 </template>
   
 <script setup>
@@ -10,8 +31,5 @@ import SaberList from './saberlist.vue'
 </script>
   
 <style scoped>
-h3 {
-    color: white;
-    font-size: 1rem;
-}
+
 </style>
